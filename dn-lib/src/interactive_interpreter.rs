8c584c6e31751b5d55a1e12317ba98c682941a77
tartasprint <tartasprint.dev@gmail.com>
2023-04-14 18:07:04 +0200

    Cleaning

--- dn-lib/src/interactive_interpreter.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("I'm the interactive interpreter.");
-    bbk_lib::test();
-}
--- dn-lib/src/proof_checker.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    println!("I'm the proof checker.");
-    bbk_lib::test();
-}
